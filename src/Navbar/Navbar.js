70fe4fdb963c8f83a09da347d96a877b5c841caa
JW Matheo <matmatvallone8@gmail.com>
2023-01-06 12:52:48 +0200

    add magnifying glass svg + create searchbar component

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./Navbar.css"
 import SofanLogoBlack from "../Assets/image/sofan_black.svg"
+import Searchbar from './Searchbar/Searchbar'
 const Navbar = () => {
   return (
     <section className='navbar-section'>
       <img className='navbar-main-logo' src={SofanLogoBlack} alt="Sofan" />
+      <Searchbar />
     </section>
   )
 }
