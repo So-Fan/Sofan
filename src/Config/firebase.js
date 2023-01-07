8f14ce19ceff6c763cc456d8a1b1bf55496dbfa0
Shajeed <shajeed@live.fr>
2023-01-07 01:04:38 -0500

    semi column

--- src/Config/firebase.js
+++ src/Config/firebase.js
@@ -22,7 +22,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
-const db = getFirestore(app)
+const db = getFirestore(app);
 
 export { analytics };
 export { auth };
