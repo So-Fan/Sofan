451b9dddf53fc31cae260b2df5fbd5df99befbf5
Shajeed <shajeed@live.fr>
2023-01-10 20:24:08 -0500

    Firebase hosting is live

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sofan
+          Sofan Web App
         </p>
       </header>
     </div>
