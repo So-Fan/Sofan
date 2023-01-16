59d395e96a8a3b776f886c98ee19021da07aff66
JW Rami <rami@jwcorp.fr>
2023-01-16 19:04:31 +0200

    added clicked like toggle

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <section className='page-container' >
+  <section className="page-container">
     <App />
-    </section>
+  </section>
 );
