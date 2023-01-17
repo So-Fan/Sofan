ef041f081e1ff97411f19dcba5179dbc14e6b73f
JW Rami <rami@jwcorp.fr>
2023-01-17 10:09:52 +0200

    first initialisation

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+import WaitingPage from "../WaitingPage/WaitingPage";
+
+function Home() {
+  return (
+    <>
+    <WaitingPage/>
+    </>
+  );
+}
+
+export default Home;
