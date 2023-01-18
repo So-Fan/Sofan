9579eeb1aacb5b9971269634c2ae689865a21314
JW Rami <rami@jwcorp.fr>
2023-01-18 16:21:46 +0200

    like button comments added + new component for likes

--- src/Components/PostsComponents/PostsComments/PostsComments.js
+++ src/Components/PostsComponents/PostsComments/PostsComments.js
@@ -1,15 +1,30 @@
 import React from "react";
 import "./PostsComments.css";
 import profilePicFan from "../../../Assets/Image/profilepicmbappecomments.svg";
+import LikeButton from "../LikeButton/LikeButton";
 function PostsComments() {
   return (
-    <div className="posts-comments-container">
-      <div className="profilepic-fan-posts-comments">
-        <img src={profilePicFan} alt="" />
-      </div>
-      <div className="fan-username-posts-comments">DonOfSomething</div>
-      PostsComments
-    </div>
+    <>
+      <>
+        <div className="posts-comments-container">
+          <div className="all-elements-of-comments">
+            <div className="profilepic-fan-posts-comments">
+              <img src={profilePicFan} alt="" />
+            </div>
+            <div className="fan-username-and-comments">
+              <div className="fan-username-posts-comments">DonOfSomething</div>
+              <div className="fan-comments-posts">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dumm…
+              </div>
+            </div>
+          </div>
+          <div className="like-posts-comments">
+            <LikeButton />
+          </div>
+        </div>
+      </>
+    </>
   );
 }
 
