453d8a276c3db3591040a722d3edc35ed7fd2476
JW Matheo <matmatvallone8@gmail.com>
2023-01-26 06:49:00 +0200

    create component

---
+++ src/Components/FeedSideNavLink/FeedSideNavLink.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./FeedSideNavLink.css"
+const FeedSideNavLink = ({href, svg, alt, title, imgWidth, gap}) => {
+  return (
+    <a href={href} className='feed-side-navlink-container'>
+      <img src={svg} alt={alt} style={{width: imgWidth}} />
+      <span style={{paddingLeft: gap}}>{title}</span>
+    </a>
+  )
+}
+
+export default FeedSideNavLink
