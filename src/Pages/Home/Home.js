296e5d1ab4eaaaee2d173ed95ea8ef8fb0901498
JW Rami <rami@jwcorp.fr>
2023-01-27 23:03:55 +0200

    display none for 800px and below, white page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -11,25 +11,27 @@ function Home({ waitingPage }) {
         </>
       ) : (
         <>
-          <h1>Home</h1>
-          <h1>
-            <ul>
-              <li>
-                <a href="http://localhost:3000/signupathlete">
-                  {" "}
-                  Sign up Athlete
-                </a>
-              </li>
-              <li>
-                <a href="http://localhost:3000/publication">
-                  Publication component
-                </a>
-              </li>
-              <li>
-                <a href="http://localhost:3000/post938098">Full Page Post</a>
-              </li>
-            </ul>
-          </h1>
+          <section className="sofan-webapp-container">
+            <h1>Home</h1>
+            <h1>
+              <ul>
+                <li>
+                  <a href="http://localhost:3000/signupathlete">
+                    {" "}
+                    Sign up Athlete
+                  </a>
+                </li>
+                <li>
+                  <a href="http://localhost:3000/publication">
+                    Publication component
+                  </a>
+                </li>
+                <li>
+                  <a href="http://localhost:3000/post938098">Full Page Post</a>
+                </li>
+              </ul>
+            </h1>
+          </section>
         </>
       )}
     </>
