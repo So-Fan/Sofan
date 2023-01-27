2b413f3216b1d36a6e04c7b3b5d488fc1ddbb728
JW Matheo <matmatvallone8@gmail.com>
2023-01-27 21:50:47 +0200

    add carroussel logic

--- src/Components/FavAthlete/AthleteTemplate/AthleteTemplate.js
+++ src/Components/FavAthlete/AthleteTemplate/AthleteTemplate.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./AthleteTemplate.css"
-const AthleteTemplate = ({href, src, alt, athleteName}) => {
+const AthleteTemplate = ({href, src, athleteName}) => {
   return (
     <a href={href} className="athleteTemplate-container"
      style={{backgroundImage: `linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, #000000 100%), url(${src})`, backgroundPosition: 'center'}}
--- src/Components/FavAthlete/Carroussel/Carroussel.js
+++ src/Components/FavAthlete/Carroussel/Carroussel.js
@@ -1,12 +1,67 @@
-import React from 'react'
-import AthleteTemplate from '../AthleteTemplate/AthleteTemplate'
-import LeBron from '../fakeData/lebron.svg'
+import React, { useEffect, useState } from "react";
+import AthleteTemplate from "../AthleteTemplate/AthleteTemplate";
+import LeBron from "../fakeData/lebron.svg";
+import Mbappe from "../fakeData/mbappe.svg";
+import RightArrow from "../../../Assets/image/right_arrow.svg";
+import "./Carroussel.css";
 const Carroussel = () => {
-  return (
-    <>
-      <AthleteTemplate href={"/profile/lebron"} src={LeBron} alt={"lebron james"} athleteName={"Lebron James"} />
-    </>
-  )
-}
+  const [rightArrowClicked, setRightArrowClicked] = useState(false);
+  const [leftArrowClicked, setLeftArrowClicked] = useState(false);
+  const [counter, setCounter] = useState(0);
 
-export default Carroussel
+  const handleRightArrowClicked = () => {
+    setRightArrowClicked(true)
+  }
+  const handleLeftArrowClicked = () => {
+    setLeftArrowClicked(true)
+  }
+
+  const style ={
+    transform: `translateX(${counter}px)`,
+    transition: "all .5s ease-in-out"
+  }
+  useEffect(() => {
+    if(rightArrowClicked){
+      setCounter(counter-120)
+      setRightArrowClicked(false)
+    }
+    if(leftArrowClicked){
+      setCounter(counter+120)
+      setLeftArrowClicked(false)
+    }
+  
+  }, [rightArrowClicked, leftArrowClicked]);
+  
+
+  return (
+    <div className="carroussel-section">
+      <div className="carroussel-athlete-wrap">
+        <div className="carroussel-athlete-subwrap" style={style}>
+          <AthleteTemplate
+            href={"/profile/lebron"}
+            src={LeBron}
+            athleteName={"Lebron James"}
+          />
+          <AthleteTemplate
+            href={"/profile/lebron"}
+            src={Mbappe}
+            athleteName={"Kylian Mbappe"}
+          />
+          <AthleteTemplate
+            href={"/profile/lebron"}
+            src={LeBron}
+            athleteName={"Lebron James"}
+          />
+        </div>
+      </div>
+      <div onClick={handleRightArrowClicked} className="carroussel-right-arrow-wrap">
+        <img src={RightArrow} alt="right arrow" />
+      </div>
+      <div onClick={handleLeftArrowClicked} className="carroussel-left-arrow-wrap">
+        <img src={RightArrow} alt="left arrow" />
+      </div>
+    </div>
+  );
+};
+
+export default Carroussel;
