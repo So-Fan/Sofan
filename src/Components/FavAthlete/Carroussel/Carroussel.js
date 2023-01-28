366e94f01e3756a4343f22df11ba495391537a32
JW Matheo <matmatvallone8@gmail.com>
2023-01-29 01:15:50 +0200

    carroussel with arrow

--- src/Components/FavAthlete/Carroussel/Carroussel.js
+++ src/Components/FavAthlete/Carroussel/Carroussel.js
@@ -10,56 +10,95 @@ const Carroussel = () => {
   const [counter, setCounter] = useState(0);
 
   const handleRightArrowClicked = () => {
-    setRightArrowClicked(true)
-  }
+    setRightArrowClicked(true);
+  };
   const handleLeftArrowClicked = () => {
-    setLeftArrowClicked(true)
-  }
+    setLeftArrowClicked(true);
+  };
 
-  const style ={
+  const style = {
+    transform: `translateX(${counter + 60}px)`,
+    transition: "all .5s ease-in-out",
+  };
+  const style2 = {
     transform: `translateX(${counter}px)`,
-    transition: "all .5s ease-in-out"
-  }
+    transition: "all .5s ease-in-out",
+  };
   useEffect(() => {
-    if(rightArrowClicked){
-      setCounter(counter-120)
-      setRightArrowClicked(false)
+    if (rightArrowClicked) {
+      setCounter(counter - 120);
+      setRightArrowClicked(false);
     }
-    if(leftArrowClicked){
-      setCounter(counter+120)
-      setLeftArrowClicked(false)
+    if (leftArrowClicked) {
+      setCounter(counter + 120);
+      setLeftArrowClicked(false);
     }
-  
   }, [rightArrowClicked, leftArrowClicked]);
-  
+
+  const fakeArray = [
+    {
+      firstName: "James",
+      surName: "Lebron",
+      img: LeBron,
+      id: 0,
+      interaction: 15,
+    },
+    {
+      firstName: "Kylian",
+      surName: "Mbappe",
+      img: Mbappe,
+      id: 1,
+      interaction: 18,
+    },
+    {
+      firstName: "James",
+      surName: "Lebron",
+      img: LeBron,
+      id: 2,
+      interaction: 12,
+    },
+    {
+      firstName: "Kylian",
+      surName: "Mbappe",
+      img: Mbappe,
+      id: 3,
+      interaction: 8,
+    },
+  ];
+  fakeArray.forEach((athlete)=> {
+    
+  })
+  const arrayLength = fakeArray.length;
 
   return (
     <div className="carroussel-section">
       <div className="carroussel-athlete-wrap">
-        <div className="carroussel-athlete-subwrap" style={style}>
-          <AthleteTemplate
-            href={"/profile/lebron"}
-            src={LeBron}
-            athleteName={"Lebron James"}
-          />
-          <AthleteTemplate
-            href={"/profile/lebron"}
-            src={Mbappe}
-            athleteName={"Kylian Mbappe"}
-          />
-          <AthleteTemplate
-            href={"/profile/lebron"}
-            src={LeBron}
-            athleteName={"Lebron James"}
-          />
+        <div className="carroussel-athlete-subwrap" style={counter !== ((arrayLength - 2) * 120) * -1 ? style2 : style}>
+          {fakeArray.map((athlete) => {
+            return (
+              <div className="athlete-template-container">
+                <AthleteTemplate
+                  href={`/profile/${athlete.id}`}
+                  src={athlete.img}
+                  athleteName={athlete.surName + " " + athlete.firstName}
+                />
+              </div>
+            );
+          })}
         </div>
       </div>
-      <div onClick={handleRightArrowClicked} className="carroussel-right-arrow-wrap">
+      {counter !== ((arrayLength - 2) * 120) * -1 &&<div
+        onClick={handleRightArrowClicked}
+        className="carroussel-right-arrow-wrap"
+      >
         <img src={RightArrow} alt="right arrow" />
-      </div>
-      <div onClick={handleLeftArrowClicked} className="carroussel-left-arrow-wrap">
+      </div>}
+      {counter <= -120 && <div
+        onClick={handleLeftArrowClicked}
+        className="carroussel-left-arrow-wrap"
+      >
         <img src={RightArrow} alt="left arrow" />
-      </div>
+      </div>}
     </div>
   );
 };
