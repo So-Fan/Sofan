09b4dd7924d4964b43385b073af404604ffdc477
JW Matheo <matmatvallone8@gmail.com>
2023-02-06 15:43:54 +0800

    add Link to navbar

--- src/Components/Navbar/NavProfile/NavProfile.js
+++ src/Components/Navbar/NavProfile/NavProfile.js
@@ -1,16 +1,17 @@
 import React from "react";
 import "./NavProfile.css";
+import {Link} from "react-router-dom"
 const NavProfile = ({ isProfileClicked, src }) => {
 
   return (
-    <div  className="navbar-navprofile-container" >
+    <div className="navbar-navprofile-container" >
       <div className="navbar-navprofile" href="/my-profile">
         <img id="navbar-user-profile-img" src={src} alt={`${src.split("/").pop().split(".")[0]} image`} />
       </div>
       {isProfileClicked && <div className="navbar-dropdown">
-        <a href="/profile">Voir profil</a>
-        <a href="/settings">Settings</a>
-        <a href="/legals">Mentions<br />légales</a>
+        <Link to="/profile">Voir profil</Link>
+        <Link to="/settings">Settings</Link>
+        <Link to="/legals">Mentions<br />légales</Link>
       </div>}
     </div>
   );
