dfaf0e31bc6fb501ad2d3196e3617a621534b9fc
shajeed <shajeed@live.fr>
2023-02-12 18:53:15 -0500

    Updated Validation.

--- src/Components/NewsLetter/NewsLetter.js
+++ src/Components/NewsLetter/NewsLetter.js
@@ -1,53 +1,88 @@
 import React, { useState, useEffect } from 'react';
 import './NewsLetter.css';
 import { db } from "../../Config/firebase";
-import { collection, addDoc } from 'firebase/firestore';
+import { collection, addDoc, query, where, getDocs } from 'firebase/firestore';
+import "firebase/firestore";
 
 const NewsLetter = () => {
   const [email, setEmail] = useState('');
   const [isValid, setIsValid] = useState(true);
-  const emailCollectionRef = collection(db, 'news_letter_email')
+  const [isSubscribed, setIsSubscribed] = useState(false);
+  const [isAlreadySubscribed, setIsAlreadySubscribed] = useState(false);
+  const emailCollectionRef = collection(db, 'news_letter_email');
 
-  const handleSubscribe = async () => {
-    if (!email || !/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i.test(email)) {
-      setIsValid(false);
-      return;
-    } 
 
+  const validateEmail = (email) => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+
+  const handleSubscribe = async (event) => {
+    event.preventDefault();
+    
+    setIsValid(validateEmail(email));
+
+    if (!isValid || !email) return;
+    let data;
     try {
-      await addDoc(emailCollectionRef, {email: email})
+      const emailRef = query(emailCollectionRef, where("email", "==", email));
+      const querySnapshot = await getDocs(emailRef);
+      //console.log(querySnapshot);
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        data = doc.data();
+      });
+      if (!data) {
+         await addDoc(emailCollectionRef, { email: email, date: new Date() })
+        setIsSubscribed(true);
+        setIsAlreadySubscribed(false)
+        // analytics.logEvent("newsletter_subscribed", { email });
+      } else {
+        setIsSubscribed(false);
+        setIsAlreadySubscribed(true)
+        setIsValid(false);
+        console.log("Already Exist");
+      }
+      
     } catch (error) {
       console.error("Error adding email to Firestore: ", error);
     }
-    // try {
-    //   await firestore
-    //     .collection("news_letter_email")
-    //     .doc(email)
-    //     .set({ email });
-    // } catch (error) {
-    //   console.error("Error adding email to Firestore: ", error);
-    // }
   };
 
-  return (
-    <div>
-      <div style={styles.message}>Soyez au courant quand Sofan sera live !</div>
-      <div style={styles.container}>
-        <input
-          className={`email-input ${!isValid ? 'Invalid' : ''} ${email && /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i.test(email) && isValid ? 'Valid' : ''}`}
-          type="email"
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-          placeholder="Entrez votre adresse email"
-          style={styles.input}
-        />
-        <button onClick={handleSubscribe} style={styles.button}>
-          Abonnez-vous
-        </button>
+  if (isAlreadySubscribed) {
+    return (
+      <div>
+        <div style={styles.message}>Il semblerait que vous etes deja abonnee avec ce email.</div>
       </div>
-    </div>
-  );
+    )
+  } else if (!isSubscribed) {
+    return (
+      <div>
+        <div style={styles.message}>Soyez au courant quand Sofan sera live !</div>
+        <div style={styles.container}>
+          <input
+            className={`email-input ${!isValid ? 'Invalid' : ''} ${email && /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i.test(email) && isValid ? 'Valid' : ''}`}
+            type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            placeholder="Entrez votre adresse email"
+            style={styles.input}
+          />
+          <button onClick={handleSubscribe} style={styles.button}>
+            Abonnez-vous
+          </button>
+        </div>
+      </div>
+    );
+  } else if (isSubscribed && !isAlreadySubscribed) {
+    return (
+      <div>
+        <div style={styles.message}>Votre adresse email a bien été reçu. Nous vous tiendrons au courant des dernières actualités de Sofan !</div>
+      </div>
+    )
+  }
 };
+
 
 const styles = {
   container: {
@@ -80,6 +115,10 @@ const styles = {
     border: 'none',
     fontSize: '16px',
     cursor: 'pointer',
+    transition: 'background-color 500ms ease-in-out, color 500ms ease-in-out',
+    ':hover': {
+      backgroundColor: '##f7c520',
+    }
   },
 };
 
