5c3a1be7e39934cebb221ea767ab4f34d15c9727
JW Rami <rami@jwcorp.fr>
2023-02-13 17:02:15 +0800

    modal component added + some css change

--- src/Components/PostsComponents/PostsFeed/PostsFeed.js
+++ src/Components/PostsComponents/PostsFeed/PostsFeed.js
@@ -5,12 +5,21 @@ import PostsDescription from "../PostsDescription/PostsDescription";
 import LikesCommentsCounter from "../LikesCommentsCounter/LikesCommentsCounter";
 import AddCommentInput from "../AddCommentInput/AddCommentInput";
 import DropDownMenu from "../DropDownMenu/DropDownMenu";
+import FullPagePost from "../../../Pages/FullPagePost/FullPagePost";
+import { Link } from "react-router-dom";
 
 function PostsFeed() {
+  const [modal, setModal] = useState(true);
   const [isDropDownButtonClicked, setIsDropDownButtonClicked] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
   function displayDropDown() {
     if (isDropDownButtonClicked) {
-      return <DropDownMenu setIsDropDownButtonClicked={setIsDropDownButtonClicked} />;
+      return (
+        <DropDownMenu setIsDropDownButtonClicked={setIsDropDownButtonClicked} />
+      );
     } else {
       return <></>;
     }
@@ -18,6 +27,19 @@ function PostsFeed() {
   return (
     <>
       <div className="publication-container">
+        {modal && (
+          <div onClick={toggleModal} className="overlay-postfeed"></div>
+        )}
+        {modal && (
+              <>
+                <FullPagePost
+                  modal={modal}
+                  setModal={setModal}
+                  toggleModal={toggleModal}
+                />
+              </>
+            )}
+
         <div className="publication-content">
           {displayDropDown()}
           <div className="publication-head-container">
@@ -32,9 +54,7 @@ function PostsFeed() {
               agePublicationPollPost="age-publication-pollpost"
             />
           </div>
-          <PostsDescription
-          postFeedDescription="post-feed-description-resize"
-          />
+          <PostsDescription postFeedDescription="post-feed-description-resize" />
           {/* Backend here */}
           <div className="publication-media">MEDIA</div>
           {/* Backend here */}
@@ -47,8 +67,17 @@ function PostsFeed() {
             likeButtonContainerPollPost="like-button-container-pollpost"
             logoCommentsPublicationPollPost="logo-comments-publication-pollpost"
           />
-          <div className="show-comments-button-publication">
-            Show 10 comments
+          <div onClick={toggleModal} className="show-comments-button-publication">
+            <Link>Show 10 comments</Link>
+            {/* {modal && (
+              <>
+                <FullPagePost
+                  modal={modal}
+                  setModal={setModal}
+                  toggleModal={toggleModal}
+                />
+              </>
+            )} */}
           </div>
           <div className="line-separation-comments-publication-container">
             <div className="line-separation-comments-publication"></div>
