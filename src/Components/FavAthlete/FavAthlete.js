3a0eefa84669b81cc6ff2a9fe8bf81f05065eb31
JW Matheo <matmatvallone8@gmail.com>
2023-02-15 18:51:25 +0800

    fully responsive feed page

--- src/Components/FavAthlete/FavAthlete.js
+++ src/Components/FavAthlete/FavAthlete.js
@@ -1,7 +1,73 @@
 import React from "react";
+import AthleteTemplate from "./AthleteTemplate/AthleteTemplate";
 import Carroussel from "./Carroussel/Carroussel";
 import "./FavAthlete.css";
+import LeBron from "./fakeData/lebron.svg";
+import Mbappe from "./fakeData/mbappe.svg";
+import FeedSuggestionTemplate from "../FeedSuggestions/FeedSuggestionTemplate/FeedSuggestionTemplate";
 const FavAthlete = () => {
+  const fakeArray = [
+    {
+      firstName: "James",
+      surName: "Lebron",
+      img: LeBron,
+      imgResponsive: "/img/sergioramos.svg",
+      id: 0,
+      interaction: 15,
+      isFan: false,
+      type: "Basketball"
+    },
+    {
+      firstName: "Kylian",
+      surName: "Mbappe",
+      img: Mbappe,
+      imgResponsive: "/img/sergioramos.svg",
+      id: 1,
+      interaction: 18,
+      isFan: true,
+      type: "Football"
+    },
+    {
+      firstName: "James2",
+      surName: "Lebron2",
+      img: LeBron,
+      imgResponsive: "/img/sergioramos.svg",
+      id: 2,
+      interaction: 12,
+      isFan: true,
+      type: "Basketball"
+    },
+    {
+      firstName: "Kylian2",
+      surName: "Mbappe2",
+      img: Mbappe,
+      imgResponsive: "/img/sergioramos.svg",
+      id: 3,
+      interaction: 8,
+      isFan: false,
+      type: "Football"
+    },
+    {
+      firstName: "Kylian3",
+      surName: "Mbappe3",
+      img: Mbappe,
+      imgResponsive: "/img/sergioramos.svg",
+      id: 4,
+      interaction: 1,
+      isFan: false,
+      type: "Football"
+    },
+  ];
+  const userFanAthlete = fakeArray.filter((athlete) => athlete.isFan === true);
+  const userRecommandationAthlete = fakeArray.filter(
+    (athlete) => athlete.isFan === false
+  );
+
+  userRecommandationAthlete.sort(function (a, b) {
+    return b.interaction - a.interaction;
+  });
+
+  const filteredArray = [...userFanAthlete, ...userRecommandationAthlete];
   return (
     <div className="favAthlete-container">
       <div className="favAthlete-horizontal"></div>
@@ -11,6 +77,16 @@ const FavAthlete = () => {
           <a href="/favathlete-details"> Voir plus</a>
         </div>
         <Carroussel />
+        <div className="favAthlete-responsive-container">
+          {filteredArray.map((athlete)  => (
+            <FeedSuggestionTemplate 
+            name={`${athlete.firstName} ${athlete.surName}`}
+            type={athlete.type}
+            athleteProfilePicture={athlete.imgResponsive}
+            id={athlete.id}
+            />
+          ))}
+        </div>
       </div>
       <div className="favAthlete-horizontal"></div>
     </div>
