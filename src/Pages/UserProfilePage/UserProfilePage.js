2e0fb8d1fd55e5c6a2c2b6079272667a57da0e8f
JW Rami <rami@jwcorp.fr>
2023-02-19 21:25:41 +0800

    creation userprofiledescription component

--- src/Pages/UserProfilePage/UserProfilePage.js
+++ src/Pages/UserProfilePage/UserProfilePage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import BannerAndProfilePic from "../../Components/BannerAndProfilePic/BannerAndProfilePic";
 import UserNameAndStats from "../../Components/UserNameAndStats/UserNameAndStats";
+import UserProfileDescription from "../../Components/UserProfileDescription/UserProfileDescription";
 import "./UserProfilePage.css";
 
 function UserProfilePage() {
@@ -11,11 +12,7 @@ function UserProfilePage() {
           <BannerAndProfilePic />
           <div className="username-stats-description">
             <UserNameAndStats />
-            <div className="user-description-container">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-              posuere tellus vehicula leo iaculis luctus. Ut vulputate elit
-              risus, eget faucibus justo consectetur in.
-            </div>
+            <UserProfileDescription/>
           </div>
         </div>
       </section>
