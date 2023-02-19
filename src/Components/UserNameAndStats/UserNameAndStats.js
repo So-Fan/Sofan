b66a93ca4114ff680dc72bcd373cce9961cf0faa
JW Rami <rami@jwcorp.fr>
2023-02-19 21:19:42 +0800

    header complete and fully responsiv

---
+++ src/Components/UserNameAndStats/UserNameAndStats.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import "./UserNameAndStats.css";
+function UserNameAndStats() {
+  const userNameAndStatsObject = {
+    athletesFollowing: 145,
+    athletesSupporting: 15,
+    nftOwned: 159,
+  };
+  return (
+    <div className="username-and-stats-container">
+      {/* 83px */}
+      <div className="username-container-userpage">Gr3goir3</div>
+      <div className="stats-user-container">
+        <div className="line-separation-userprofile"></div>
+        <div className="following-athlete-container">
+          <div className="following-athlete-number">
+            {userNameAndStatsObject.athletesFollowing}
+          </div>
+          <div className="following-athlete-title">
+            <div id="athlete-title-userpage">Athletes</div>
+            <span>following</span>
+          </div>
+        </div>
+        <div className="line-separation-userprofile"></div>
+        <div className="athlete-supporting-container">
+          <div className="athlete-supporting-number">
+            {userNameAndStatsObject.athletesSupporting}
+          </div>
+          <div className="athlete-supporting-title">
+            <div id="athlete-title-userpage">Athletes</div>
+            <span>supporting</span>
+          </div>
+        </div>
+        <div className="line-separation-userprofile"></div>
+        <div className="counter-nft-owned-user-container">
+          <div className="counter-nft-owned-user-number">
+            {userNameAndStatsObject.nftOwned}
+          </div>
+          <div className="counter-nft-owner-user-title">NFT</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UserNameAndStats;
--- src/Pages/UserProfilePage/UserProfilePage.js
+++ src/Pages/UserProfilePage/UserProfilePage.js
@@ -1,15 +1,26 @@
-import React from 'react'
-import BannerAndProfilePic from '../../Components/BannerAndProfilePic/BannerAndProfilePic'
-import "./UserProfilePage.css"
+import React from "react";
+import BannerAndProfilePic from "../../Components/BannerAndProfilePic/BannerAndProfilePic";
+import UserNameAndStats from "../../Components/UserNameAndStats/UserNameAndStats";
+import "./UserProfilePage.css";
+
 function UserProfilePage() {
   return (
     <>
-    <section className='userprofilepage-container'>
-
-    <BannerAndProfilePic/>
-    </section>
+      <section className="userprofilepage-container">
+        <div className="userheader-container">
+          <BannerAndProfilePic />
+          <div className="username-stats-description">
+            <UserNameAndStats />
+            <div className="user-description-container">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              posuere tellus vehicula leo iaculis luctus. Ut vulputate elit
+              risus, eget faucibus justo consectetur in.
+            </div>
+          </div>
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default UserProfilePage
+export default UserProfilePage;
