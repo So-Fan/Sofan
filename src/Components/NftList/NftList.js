e8bcc4f273da71ba7bf00d2e6918df3cef229c93
JW Rami <rami@jwcorp.fr>
2023-02-21 14:03:06 +0800

    nft list component created

---
+++ src/Components/NftList/NftList.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./NftList.css"
+import explorePicture from "../../Assets/Image/explorepicture.svg"
+function NftList() {
+  return (
+    <div className="user-nft-transfer-container">
+        <div>Mint</div>
+        <div className="user-nft-picture-and-title">
+          <img src={explorePicture} alt="nft picture" />
+          <div className="collection-name-nft-id-user-activity">
+            <span>Explore the World with Alexia...</span>
+            <span>#393</span>
+          </div>
+        </div>
+        <div className="nft-price-user-activity">
+          <div className="nftc-price-eth-user-activity">0.50 ETH</div>
+          <div className="nft-price-eur-user-activity">692.04€</div>
+        </div>
+        <div className="nft-quantity-user-activity">1</div>
+        <div className="nft-from-user-activity">Alexia Barrier</div>
+        <div className="nft-to-user-activity">Gr3goir3</div>
+        <div className="nft-date-transfer-user-activity">1 hour ago</div>
+      </div>
+  )
+}
+
+export default NftList
--- src/Components/UserActivity/UserActivity.js
+++ src/Components/UserActivity/UserActivity.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./UserActivity.css";
 import explorePicture from "../../Assets/Image/explorepicture.svg";
+import NftList from "../NftList/NftList";
 function UserActivity() {
   return (
     <div className="user-activity-container">
@@ -14,26 +15,7 @@ function UserActivity() {
         <div className="user-date-type-activity">Date</div>
       </div>
       <div className="separation-line-horizontal-userprofile"></div>
-      <div className="user-nft-transfer-container">
-        <div>Mint</div>
-        <div className="user-nft-picture-and-title">
-          <img src={explorePicture} alt="nft picture" />
-          {/* <div> */}
-          <div className="collection-name-nft-id-user-activity">
-            <span>Explore the World with Alexia...</span>
-            <span>#393</span>
-          </div>
-          {/* </div> */}
-        </div>
-        <div className="nft-price-user-activity">
-          <div className="nftc-price-eth-user-activity">0.50 ETH</div>
-          <div className="nft-price-eur-user-activity">692.04€</div>
-        </div>
-        <div className="nft-quantity-user-activity">1</div>
-        <div className="nft-from-user-activity">Alexia Barrier</div>
-        <div className="nft-to-user-activity">Gr3goir3</div>
-        <div className="nft-date-transfer-user-activity">1 hour ago</div>
-      </div>
+      <NftList/>
     </div>
   );
 }
