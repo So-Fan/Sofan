8cbf6816dabb88ef22b8853d95d7868aa5c97359
JW Rami <rami@jwcorp.fr>
2023-02-25 21:59:10 +0800

    scroll added

--- src/Components/UserProfileComponents/ReceivedOffers/ReceivedOffers.js
+++ src/Components/UserProfileComponents/ReceivedOffers/ReceivedOffers.js
@@ -3,17 +3,16 @@ import DataTitles from "../DataTitles/DataTitles";
 import NftList from "../NftList/NftList";
 import "./ReceivedOffers.css";
 
-function ReceivedOffers({
-    userFrom,
-}) {
-    return (
-    <section    className="received-offers-user-container">
+function ReceivedOffers({ userFrom }) {
+  const nftListArray = Array.from({ length: 159 });
+  return (
+    <section className="received-offers-user-container">
       {/* class à rename en bas */}
-      <DataTitles 
+      <DataTitles
         yourOffersTitle="Offers"
         offersFromTitle="From"
         offersToTitle="To"
-        // 
+        //
         offersNftTitleClass="formulated-offers-nft-title"
         yourOffersTitleClass="received-offers-offers-title"
         offersFromTitleClass="received-offers-from-title"
@@ -21,19 +20,24 @@ function ReceivedOffers({
         offersDateTitleClass="received-offers-date-title"
         // Nft data list
       />
-      <NftList
-        offersDisplaySourceTypeClass="formulated-offers-display-source-type"
-        receivedFrom={userFrom}
-        offersTo="Gr3goir3"
-        //
-        offersNftContentClass="received-offers-nft-picture-and-title"
-        offersYourOffersPriceClass="received-offers-offers-price"
-        offersFromClass="received-offers-from"
-        offersToClass="received-offers-to"
-        offersDateClass="received-offers-date"  
-        offersDeclineClass="received-offers-decline"
-        offersAcceptClass="received-offers-accept"
-      />
+      <div className="received-offers-nft-list-container">
+        {nftListArray.map((_, index) => (
+          <NftList
+            key={index}
+            offersDisplaySourceTypeClass="formulated-offers-display-source-type"
+            receivedFrom={userFrom}
+            offersTo="Gr3goir3"
+            //
+            offersNftContentClass="received-offers-nft-picture-and-title"
+            offersYourOffersPriceClass="received-offers-offers-price"
+            offersFromClass="received-offers-from"
+            offersToClass="received-offers-to"
+            offersDateClass="received-offers-date"
+            offersDeclineClass="received-offers-decline"
+            offersAcceptClass="received-offers-accept"
+          />
+        ))}
+      </div>
     </section>
   );
 }
