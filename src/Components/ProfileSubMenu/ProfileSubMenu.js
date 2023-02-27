b459d56744110f4cbf04dce005eedbdf9d93d113
JW Rami <rami@jwcorp.fr>
2023-02-27 18:46:58 +0800

    fix submenu active sub

--- src/Components/ProfileSubMenu/ProfileSubMenu.js
+++ src/Components/ProfileSubMenu/ProfileSubMenu.js
@@ -20,17 +20,17 @@ const ProfileSubMenu = ({isProfileSubMenuButtonClicked, setIsProfileSubMenuButto
     return (
     <div className="profileSubMenu-component">
       <div className={isProfileSubMenuButtonClicked[0] ? "profilesubmenu-wrap-clicked profilesubmenu-wrap" : "profilesubmenu-wrap"}>
-        <Button onClick={handleProfileSubMenuButtonClicked} text="Collected NFT" style={ProfileSubMenuButtonStyle.styling}/>
+        <Button onClick={handleProfileSubMenuButtonClicked} text="Collected NFT" style={isProfileSubMenuButtonClicked[0] ? ProfileSubMenuButtonStyle.stylingClicked : ProfileSubMenuButtonStyle.stylingNotClicked}/>
       </div>
       <div className={isProfileSubMenuButtonClicked[1] ? "profilesubmenu-wrap-clicked profilesubmenu-wrap" : "profilesubmenu-wrap"}>
-        <Button onClick={handleProfileSubMenuButtonClicked} text="Activity" style={ProfileSubMenuButtonStyle.styling}/>
+        <Button onClick={handleProfileSubMenuButtonClicked} text="Activity" style={isProfileSubMenuButtonClicked[1] ? ProfileSubMenuButtonStyle.stylingClicked : ProfileSubMenuButtonStyle.stylingNotClicked}/>
       </div>
       <div className={isProfileSubMenuButtonClicked[2] ? "profilesubmenu-wrap-clicked profilesubmenu-wrap profilesubmenu-arrow-wrap" : "profilesubmenu-wrap profilesubmenu-arrow-wrap"}>
-        <Button onClick={handleProfileSubMenuButtonClicked} text="Offres formulées" style={ProfileSubMenuButtonStyle.styling}/>
+        <Button onClick={handleProfileSubMenuButtonClicked} text="Offres formulées" style={isProfileSubMenuButtonClicked[2] ? ProfileSubMenuButtonStyle.stylingClicked : ProfileSubMenuButtonStyle.stylingNotClicked}/>
         <img src={Arrow} alt="Arrow bottom" />
       </div>
       <div className={isProfileSubMenuButtonClicked[3] ? "profilesubmenu-wrap-clicked profilesubmenu-wrap profilesubmenu-arrow-wrap" : "profilesubmenu-wrap profilesubmenu-arrow-wrap"}>
-        <Button onClick={handleProfileSubMenuButtonClicked} text="Offres reçues" style={ProfileSubMenuButtonStyle.styling}/>
+        <Button onClick={handleProfileSubMenuButtonClicked} text="Offres reçues" style={isProfileSubMenuButtonClicked[3] ? ProfileSubMenuButtonStyle.stylingClicked : ProfileSubMenuButtonStyle.stylingNotClicked}/>
         <img src={Arrow} alt="Arrow bottom" />
       </div>
     </div>
@@ -40,14 +40,23 @@ const ProfileSubMenu = ({isProfileSubMenuButtonClicked, setIsProfileSubMenuButto
 export default ProfileSubMenu;
 
 const ProfileSubMenuButtonStyle = {
-  styling: {
+  stylingNotClicked: {
     backgroundColor: "transparent",
     border: "none",
     height: "100%",
     fontFamily: 'Britanica-Heavy',
     fontSize: "20px",
     fontWeight: "400",
-    // position: "absolute",
+    opacity:"0.5"
+  },
+  stylingClicked: {
+    backgroundColor: "transparent",
+    border: "none",
+    height: "100%",
+    fontFamily: 'Britanica-Heavy',
+    fontSize: "20px",
+    fontWeight: "400",
+    opacity:"1"
   },
   //   pseudoClasses:
   //     ".button-component:hover{border-bottom: 4px solid #F8DB4A !important}",
