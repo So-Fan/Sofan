0d95ed7c588ab57d9982e803def557a3f5c16abe
JW Matheo <matmatvallone8@gmail.com>
2023-03-01 23:28:21 +0800

    add fake data, add props, add JSX, add CSS

--- src/Pages/Test/Test.js
+++ src/Pages/Test/Test.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import AthleteProfileNFTCollection from '../../Components/AthleteProfileNFTCollection/AthleteProfileNFTCollection'
+import AthleteProfileRanking from '../../Components/AthleteProfileRanking/AthleteProfileRanking'
 import "./Test.css"
 const Test = () => {
   return (
     <>
-      <AthleteProfileNFTCollection />
+      <AthleteProfileRanking />
     </>
   )
 }
