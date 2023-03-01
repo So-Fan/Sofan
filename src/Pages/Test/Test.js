1621c5f9c9ba6e816b5489ffb50e661a20153c45
JW Matheo <matmatvallone8@gmail.com>
2023-03-02 00:59:12 +0800

    add fake data, add props, add JSX, add CSS, add logic

--- src/Pages/Test/Test.js
+++ src/Pages/Test/Test.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import AthleteProfileNFTCollection from '../../Components/AthleteProfileNFTCollection/AthleteProfileNFTCollection'
 import AthleteProfileRanking from '../../Components/AthleteProfileRanking/AthleteProfileRanking'
+import NotificationPopUp from '../../Components/Navbar/NotificationPopUp/NotificationPopUp'
 import "./Test.css"
 const Test = () => {
   return (
     <>
-      <AthleteProfileRanking />
+      <NotificationPopUp />
     </>
   )
 }
