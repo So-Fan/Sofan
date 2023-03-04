27e04b2d7106e8cb92892718fae80f2bea21b826
JW Rami <rami@jwcorp.fr>
2023-03-05 07:24:36 +0800

    creator and owner data

--- src/Components/NftCollectionHeader/NftCollectionHeader.js
+++ src/Components/NftCollectionHeader/NftCollectionHeader.js
@@ -4,6 +4,18 @@ import explorePicture from "../../Assets/Image/explorepicture.svg";
 import DropDownButtonMenu from "../PostsComponents/DropDownButtonMenu/DropDownButtonMenu";
 
 function NftCollectionHeader() {
+  const dataBackendNftCollection = [
+    {
+      collectioName: "Explore the world with Alexia Barrier",
+      nftNumber: "#390",
+      creatorProfilePic:
+        "https://www.vendeeglobe.org/medias/05/06/50614/alexia-barrier-c-462-560.jpg",
+      creatorName: "Alexia Barrier",
+      ownerName: "DonOfSomething",
+      ownerProfilePic:
+        "https://storage.googleapis.com/nftimagebucket/tokens/0x8a90cab2b38dba80c64b7734e58ee1db38b8992e/preview/8970.png",
+    },
+  ];
   return (
     <section className="nft-collection-header-container">
       <div className="nft-collection-header-picture">
@@ -13,21 +25,59 @@ function NftCollectionHeader() {
         <div className="nft-collection-header-data-and-buy-module-content">
           <div className="nft-collection-header-collection-name-and-number">
             <div className="nft-collection-header-collection-name-title">
-              <p>Explore the world with Alexia Barrier</p>
-              <div>
+              <p>{dataBackendNftCollection[0].collectioName}</p>
+              <div className="nft-collection-header-dropdown-button">
                 <span></span>
                 <span></span>
                 <span></span>
-                {/* <button
-                  // onClick={handleDropdownPostFeedClick}
-                  className="nft-collection-header-dropdown-menu-button-container"
-                >
-                  <div className="nft-collection-header-dropdown-button-publication">
-                    <div className="nft-collection-header-dropdown-button-point"></div>
-                    <div className="nft-collection-header-dropdown-button-point"></div>
-                    <div className="nft-collection-header-dropdown-button-point"></div>
-                  </div>
-                </button> */}
+              </div>
+            </div>
+            <div className="nft-collection-header-nft-number">
+              {dataBackendNftCollection[0].nftNumber}
+            </div>
+          </div>
+        </div>
+        <div className="nft-collection-header-owner-creator-container">
+          <div className="nft-collection-header-owner-creator-wrap">
+            <div>
+              <img
+                src={dataBackendNftCollection[0].creatorProfilePic}
+                alt="Photo de profile Createur"
+              />
+            </div>
+            <div className="nft-collection-header-owner-creator-title-and-name-creator">
+              <span className="nft-collection-header-owner-creator-title-creator">
+                Creator
+              </span>
+              <span className="nft-collection-header-owner-creator-name-creator">
+                {dataBackendNftCollection[0].creatorName}
+              </span>
+            </div>
+            <div>
+              <img
+                src={dataBackendNftCollection[0].ownerProfilePic}
+                alt="Photo de profile Propriétaire"
+              />
+            </div>
+            <div className="nft-collection-header-owner-creator-title-and-name-owner">
+              <span className="nft-collection-header-owner-creator-title-owner">
+                Owner
+              </span>
+              <span className="nft-collection-header-owner-creator-name-owner">
+                {dataBackendNftCollection[0].ownerName}
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="nft-collection-header-line-separation"></div>
+        <div className="nft-collection-header-buy-module-container">
+          <div className="nft-collection-header-buy-module-wrap">
+            <div className="nft-collection-header-price-and-bid-container">
+              <div className="nft-collection-header-price">
+
+              </div>
+              <div className="nft-collection-header-bid">
+
               </div>
             </div>
           </div>
