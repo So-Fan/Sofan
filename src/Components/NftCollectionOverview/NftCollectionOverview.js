6871f4de6800fed588090253a6ad3983190b25ea
JW Rami <rami@jwcorp.fr>
2023-03-06 15:59:02 +0800

    first utilities finish

--- src/Components/NftCollectionOverview/NftCollectionOverview.js
+++ src/Components/NftCollectionOverview/NftCollectionOverview.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./NftCollectionOverview.css";
-import meetingsLogo from "../../Assets/Image/meetings-logo.svg"
-import liveLogo from "../../Assets/Image/live-logo.svg"
-import merchLogo from "../../Assets/Image/merch-logo.svg"
+import meetingsLogo from "../../Assets/Image/meetings-logo.svg";
+import liveLogo from "../../Assets/Image/live-logo.svg";
+import merchLogo from "../../Assets/Image/merch-logo.svg";
 
 function NftCollectionOverview() {
   return (
@@ -10,9 +10,31 @@ function NftCollectionOverview() {
       <div className="nft-collection-overview-utilities-container">
         <div className="nft-collection-overview-utilities-title">Utilities</div>
         <div className="nft-collection-overview-utilities-one">
-            <div className="nft-collection-overview-utilities-one-header"></div>
-            <div className="nft-collection-overview-utilities-one-description"></div>
-            <div className="nft-collection-overview-utilities-one-date"></div>
+          <div className="nft-collection-overview-utilities-one-wrap">
+            <div className="nft-collection-overview-utilities-one-header">
+              <div className="nft-collection-overview-one-header-logo">
+                <img src={meetingsLogo} alt="logo contrepartie rencontre fan" />
+              </div>
+              <div className="nft-collection-overview-one-header-title">
+                Meeting with Alexia
+              </div>
+              <div className="nft-collection-overview-one-header-status">
+                <div className="nft-collectin-overview-one-header-status-color"></div>
+                <div className="nft-collection-overview-one-header-status-text">
+                  Available
+                </div>
+              </div>
+            </div>
+            <div className="nft-collection-overview-utilities-one-description">
+              At the end of the Vendée Globe 2023, 15/400 of the holders of this
+              NFT will have the chance to meet Alexia Barrier personally at the
+              finish of the
+              coursemaklemlakemazlkmalzkazemlkazemleakazlmekazelmaezkaz
+            </div>
+            <div className="nft-collection-overview-utilities-one-date">
+              Date of the utility : July 9th 2023
+            </div>
+          </div>
         </div>
         <div className="nft-collection-overview-utilities-two"></div>
         <div className="nft-collection-overview-utilities-three"></div>
