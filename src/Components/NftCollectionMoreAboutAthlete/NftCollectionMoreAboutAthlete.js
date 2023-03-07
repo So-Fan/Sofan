e9204244fa476ced18afa0d2f36775187cb3ea35
JW Rami <rami@jwcorp.fr>
2023-03-08 07:40:23 +0800

    more about athlete finished fully responsive

--- src/Components/NftCollectionMoreAboutAthlete/NftCollectionMoreAboutAthlete.js
+++ src/Components/NftCollectionMoreAboutAthlete/NftCollectionMoreAboutAthlete.js
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import "./NftCollectionMoreAboutAthlete.css";
 
 function NftCollectionMoreAboutAthlete() {
-//   const [isExpanded, setIsExpanded] = useState(true);
-
   const backendDataNftCollection = {
     moreAbout: [
       {
@@ -12,34 +10,41 @@ function NftCollectionMoreAboutAthlete() {
         fullName: "Alexia Barrier",
         description:
           "Alexia Barrier is a French professional sailor and skipper. She has competed in several sailing races, including the Solitaire du Figaro and the Vendée Globe. She is the first woman to have completed the Vendée Globe solo non-stop race, finishing in 16th place. She is also the founder of the Sail & Help association, which promotes environmental protection and education through sailing.",
-        fansNumber: "",
+        fansNumber: 121023,
       },
     ],
   };
 
-//   const handleToggle = () => {
-//     setIsExpanded((prevState) => !prevState);
-//   };
-
   return (
     <section className="nft-collection-more-about-athlete-container">
-      <div
-        className="nft-collection-more-about-athlete-wrap"
-      >
+        <div className="nft-collection-more-about-athlete-title">
+            More about the athlete
+        </div>
+      <div className="nft-collection-more-about-athlete-wrap">
         <div className="nft-collection-more-about-athlete-picture-container">
           <img
             src={backendDataNftCollection.moreAbout[0].picture}
             alt="photo athlete"
           />
         </div>
-        <div className="nft-collection-more-about-presentation-container">
-          {/* <h2>{backendDataNftCollection.moreAbout[0].fullName}</h2> */}
-          {/* <p>{backendDataNftCollection.moreAbout[0].description}</p> */}
+        <div className="nft-collection-more-about-athlete-presentation-container">
+          <div className="nft-collection-more-about-athete-sport-title">
+            Skipper
+          </div>
+          <div className="nft-collection-more-about-athlete-name">
+            {backendDataNftCollection.moreAbout[0].fullName}
+          </div>
+          <div className="nft-collection-more-about-athlete-description">
+            {backendDataNftCollection.moreAbout[0].description}
+          </div>
+          <div className="nft-collection-more-about-athlete-fans-count">
+            {backendDataNftCollection.moreAbout[0].fansNumber} fans
+          </div>
+          <button className="nft-collection-more-about-athlete-view-profile-button">
+            See the profile
+          </button>
         </div>
       </div>
-      {/* <button onClick={handleToggle}>
-        {isExpanded ? "Show less" : "Show more"}
-      </button> */}
     </section>
   );
 }
