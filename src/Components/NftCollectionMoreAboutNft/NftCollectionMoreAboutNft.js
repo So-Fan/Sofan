41df6341bb3f66b49b9695083b39e8e76b3e6a33
JW Rami <rami@jwcorp.fr>
2023-03-09 08:39:51 +0800

    full responsive / added props to nft card for class change

--- src/Components/NftCollectionMoreAboutNft/NftCollectionMoreAboutNft.js
+++ src/Components/NftCollectionMoreAboutNft/NftCollectionMoreAboutNft.js
@@ -37,42 +37,31 @@ function NftCollectionMoreAboutNft() {
         ethPrice: 0.61,
         highestBidEth: 0.03,
       },
-      {
-        nftImage:
-          "https://i.seadn.io/gcs/files/dac49567c59c19147360a378c38b485d.jpg?w=500&auto=format",
-        collectionName: "Explore the World with Alexia Barrier",
-        nftId: 390,
-        ethPrice: 0.61,
-        highestBidEth: 0.03,
-      },
     ],
   };
   return (
     <section className="nft-collection-more-about-nft-container">
-      <div className="nft-collection-more-about-nft-title-and-button">
-        <div>More about the NFT collection</div>
-        <div className="nft-collection-more-about-title-button">
-          <button>Voir tout</button>
+      <div className="nft-collection-more-about-nft-wrap">
+        <div className="nft-collection-more-about-nft-title-and-button">
+          <div className="nft-collection-more-about-nft-title">
+            More about the NFT collection
+          </div>
+          <div className="nft-collection-more-about-title-button">
+            <button>Voir tout</button>
+          </div>
         </div>
-      </div>
-      <div className="nft-collection-more-about-nft-card-container">
-        {dataBackendNftCollectionMoreAboutNft.nftCard.map((element) => (
-
-        <NftCardTemplate
-          img={dataBackendNftCollectionMoreAboutNft.nftCard[0].nftImage}
-          title={dataBackendNftCollectionMoreAboutNft.nftCard[0].collectionName}
-          id={dataBackendNftCollectionMoreAboutNft.nftCard[0].nftId}
-          price={dataBackendNftCollectionMoreAboutNft.nftCard[0].ethPrice}
-          bid={dataBackendNftCollectionMoreAboutNft.nftCard[0].highestBidEth}
-        />
-        ))}
-        {/* <NftCardTemplate
-          img={dataBackendNftCollectionMoreAboutNft.nftCard[0].nftImage}
-          title={dataBackendNftCollectionMoreAboutNft.nftCard[0].collectionName}
-          id={dataBackendNftCollectionMoreAboutNft.nftCard[0].nftId}
-          price={dataBackendNftCollectionMoreAboutNft.nftCard[0].ethPrice}
-          bid={dataBackendNftCollectionMoreAboutNft.nftCard[0].highestBidEth}
-        /> */}
+        <div className="nft-collection-more-about-nft-card-container">
+          {dataBackendNftCollectionMoreAboutNft.nftCard.map((element) => (
+            <NftCardTemplate
+            fontStyle="nftcardtemplate-container-content-title-props"
+              img={element.nftImage}
+              title={element.collectionName}
+              id={element.nftId}
+              price={element.ethPrice}
+              bid={element.highestBidEth}
+            />
+          ))}
+        </div>
       </div>
     </section>
   );
