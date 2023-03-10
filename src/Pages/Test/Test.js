0b952b45899aad1282281a403b957cd75f0a0687
JW Matheo <matmatvallone8@gmail.com>
2023-03-10 19:58:13 +0800

    add responsive and change classname

--- src/Pages/Test/Test.js
+++ src/Pages/Test/Test.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React from "react";
+import NftCollectionHeader from "../../Components/NftCollectionHeader/NftCollectionHeader";
 import "./Test.css";
 const Test = () => {
- 
+  // Les 4 dernieres stats proviennent d'Opensea
+  const data = {
+    collections: [
+      {
+        athleteName: "Alexia Barrier",
+        collectionName: "Explore the world with Alexia Barrier",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris posuere tellus vehicula leo iaculis luctus. Ut vulputate elit risus, eget faucibus justo consectetur in.",
+        banner: "https://i.imgur.com/6ozImSk.png",
+        profilePicture: "https://i.imgur.com/grRujbB.png",
+        socials: {
+          discord: "https://discord.com",
+          twitter: "https://twitter.com",
+          instagram: "https://instagram.com",
+        },
+        floorPrice: "0.3",
+        AveragePrice: "0.91",
+        Owners: "7291",
+        Volume: "19234",
+      },
+    ],
+  };
   return (
     <>
-
+      <NftCollectionHeader collectionInfo={data.collections[0]} />
     </>
   );
 };
