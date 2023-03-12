e983cf8f40a25cda1a92d43dec567d30b3d58308
JW Rami <rami@jwcorp.fr>
2023-03-13 01:05:03 +0800

    update, bug to fix after display nft cards

--- src/Pages/Test/Test.js
+++ src/Pages/Test/Test.js
@@ -1,10 +1,12 @@
 import React from "react";
 import NftCollectionMoreAboutNft from "../../Components/NftCollectionMoreAboutNft/NftCollectionMoreAboutNft";
+import NftCollectionPageItems from "../../Components/NftCollectionPageItems/NftCollectionPageItems";
 import "./Test.css";
 const Test = () => {
   return (
     <>
-      <NftCollectionMoreAboutNft />
+      {/* <NftCollectionMoreAboutNft /> */}
+      <NftCollectionPageItems/>
     </>
   );
 };
