ac0682d519a5e4b4cb9dc761d0e6ccd4c84f8c44
JW Matheo <matmatvallone8@gmail.com>
2023-03-15 19:30:29 +0800

    Bid compoenent

--- src/Pages/Test/Test.js
+++ src/Pages/Test/Test.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import PopUpBuyNft from '../../Components/PopUpBuyNft/PopUpBuyNft';
+import PopUpPlaceBid from '../../Components/PopUpPlaceBid/PopUpPlaceBid';
 import "./Test.css";
 const Test = () => {
  
   return (
     <>
-      <PopUpBuyNft />
+      <PopUpPlaceBid />
     </>
   );
 };
