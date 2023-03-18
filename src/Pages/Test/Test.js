4f38a53f2b10ba8176f58d9cd105bedfde843a9b
JW Rami <rami@jwcorp.fr>
2023-03-19 04:31:12 +0800

    dashboard my events finish

--- src/Pages/Test/Test.js
+++ src/Pages/Test/Test.js
@@ -1,8 +1,10 @@
 import React from "react";
+import DashboardMyEvents from "../../Components/DashboardMyEvents/DashboardMyEvents";
 import "./Test.css";
 const Test = () => {
   return (
     <>
+    <DashboardMyEvents/>
     </>
   );
 };
