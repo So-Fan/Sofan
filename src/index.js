453415fca3ff2b1959872b0f66a3e97795cc9740
JWMatheo <88055282+JWMatheo@users.noreply.github.com>
2023-03-21 02:04:09 +0200

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-
-
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
