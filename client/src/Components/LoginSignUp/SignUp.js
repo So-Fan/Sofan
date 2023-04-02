92fe1f220d94c74e08e29c72af6a23218d57995c
Shajeed <shajeed@live.fr>
2023-04-02 16:55:50 -0400

    SignUp

--- client/src/Components/LoginSignUp/SignUp.js
+++ client/src/Components/LoginSignUp/SignUp.js
@@ -1,6 +1,28 @@
 import React from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../Configs/firebase";
+import { useNavigate } from "react-router-dom";
 
 function SignUp(props) {
+
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        navigate("/")
+        setError(false);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        setError(true);
+        // ..
+      });
+  };
+
   return (
     <div class="form-container register-container">
       <form action="#">
@@ -9,7 +31,7 @@ function SignUp(props) {
         <input type="text" placeholder="Username" />
         <input type="email" placeholder="Email" />
         <input type="password" placeholder="Mot de passe" />
-        <button>S'inscrire</button>
+        <button onClick={handleSignUp}>S'inscrire</button>
         <span>ou utilisez votre compte</span>
         <div class="social-container">
           <a href="#" class="social">
