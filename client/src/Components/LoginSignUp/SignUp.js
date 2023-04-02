e3e7d6b092a9949c91ccf8cc863900ee1b36a43f
Shajeed <shajeed@live.fr>
2023-04-02 17:04:20 -0400

    signuppp

--- client/src/Components/LoginSignUp/SignUp.js
+++ client/src/Components/LoginSignUp/SignUp.js
@@ -1,37 +1,58 @@
-import React from "react";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../Configs/firebase";
 import { useNavigate } from "react-router-dom";
 
 function SignUp(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
 
   const handleSignUp = (e) => {
     e.preventDefault();
-    createUserWithEmailAndPassword(email, password)
+    const auth = getAuth();
+
+    createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        // Signed in
+        // User signed up successfully
         const user = userCredential.user;
-        navigate("/")
-        setError(false);
+        console.log(user);
       })
       .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        setError(true);
-        // ..
+        // Handle errors here
+        setError(error.message);
+        console.error(error);
       });
   };
 
   return (
     <div class="form-container register-container">
-      <form action="#">
+      <form action="#" onSubmit={handleSignUp}>
         <h1>Inscrivez-vous ici.</h1>
         <input type="text" placeholder="Nom" />
         <input type="text" placeholder="Username" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Mot de passe" />
-        <button onClick={handleSignUp}>S'inscrire</button>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={handleEmailChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+        <button>S'inscrire</button>
         <span>ou utilisez votre compte</span>
         <div class="social-container">
           <a href="#" class="social">
