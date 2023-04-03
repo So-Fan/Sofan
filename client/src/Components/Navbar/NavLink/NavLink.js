f91d153f6f0028f0a21d5ce369c4cd77268b9e03
JW Rami <rami@jwcorp.fr>
2023-04-03 15:30:42 +0800

    hover link navbar

--- client/src/Components/Navbar/NavLink/NavLink.js
+++ client/src/Components/Navbar/NavLink/NavLink.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const NavLink = ({ name, link }) => {
   return (
     <Link className="navbar-navicon" to={link}>
-      <div className="navbar-navicon">
+      <div className="navbar-navicon-name">
 
       {name}
       </div>
