03181b6655471bf420aad1a98c5de28c545e0cde
Ra <106039941+JW-Rami@users.noreply.github.com>
2023-04-19 06:07:08 +0200

    test gpg windows

--- client/src/Components/PopUpConfirmationOffer/PopUpConfirmationOffer.js
+++ client/src/Components/PopUpConfirmationOffer/PopUpConfirmationOffer.js
@@ -3,27 +3,7 @@ import "./PopUpConfirmationOffer.css";
 import { Network, Alchemy } from "alchemy-sdk";
 
 function PopUpConfirmationOffer() {
-  //   const [nftMetadataApi, setNftMetadataApi] = useState();
 
-  //   // Api Alchemy setup
-  //   const settings = {
-  //     apiKey: "34lcNFh-vbBqL9ignec_nN40qLHVOfSo",
-  //     network: Network.ETH_MAINNET,
-  //     maxRetries: 10,
-  //   };
-  //   const alchemy = new Alchemy(settings);
-
-  //   async function getNftsMetadata() {
-  //     const metadata = await alchemy.nft.getContractMetadata(
-  //       "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d",
-  //       "7603"
-  //     );
-  //     setNftMetadataApi(metadata);
-  //   }
-  //   useEffect(() => {
-  //     getNftsMetadata();
-  //   }, []);
-  // console.log(nftMetadataApi);
   return (
     <div className="popup-confirmation-offer-container">
       <div className="popup-confirmation-offer-wrap">
