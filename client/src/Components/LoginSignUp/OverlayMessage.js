04ea8913bd552c31f98204b4caaf044d0878add4
Ra <rami@jwcorp.fr>
2023-04-24 05:52:46 +0200

    css change

--- client/src/Components/LoginSignUp/OverlayMessage.js
+++ client/src/Components/LoginSignUp/OverlayMessage.js
@@ -6,11 +6,10 @@ function OverlayMessage({ onClickRegister, onClickLogin }) {
       <div class="overlay">
         <div class="overlay-panel overlay-left">
           <h1 class="title">
-            Bonjour <br />
-            les amis
+           Déjà un compte ? <br />
           </h1>
-          <p>Si vous avez un compte, connectez-vous ici et amusez-vous</p>
-          <button class="ghost" id="login" onClick={onClickLogin}>
+          <p>Si vous avez un compte, connectez-vous ici.</p>
+          <button class="ghost overlay-login-button" id="login" onClick={onClickLogin}>
             Se connecter
             <i class="lni lni-arrow-left login"></i>
           </button>
@@ -23,7 +22,7 @@ function OverlayMessage({ onClickRegister, onClickLogin }) {
           <p>
             Si vous n'avez pas encore de compte, rejoignez-nous et commencez votre voyage.
           </p>
-          <button class="ghost" id="register" onClick={onClickRegister}>
+          <button class="ghost-register" id="register" onClick={onClickRegister}>
             S'inscrire
             <i class="lni lni-arrow-right register"></i>
           </button>
