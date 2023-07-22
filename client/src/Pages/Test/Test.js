31742d1cd7e76f00ac02fdd18caea8cae03b47f0
JWMatheo <matmatvallone8@gmail.com>
2023-07-22 23:56:53 +0200

    add button for userv iew

--- client/src/Pages/Test/Test.js
+++ client/src/Pages/Test/Test.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState, useCallback } from "react";
 import "./Test.css";
 import PopUpEditProfile from "../../Components/PopUpEditProfile/PopUpEditProfile";
+import NftCollectionHeader from "../../Components/NftCollectionHeader/NftCollectionHeader";
+import NftSingle from "../NftSingle/NftSingle";
 const Test = () => {
 
   return (
     <>
-     <PopUpEditProfile />
+     <NftSingle />
     </>
   );
 };
