72f06501d670392abcea33ae6e7c3fc382e12fae
Ra <rami@jwcorp.fr>
2023-07-31 01:27:26 +0200

    update

---
+++ client/src/Pages/ErrorPage/ErrorPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./ErrorPage.css"
+function ErrorPage() {
+  return (
+    <div className='error-page-container'>
+      <div className='error page-wrap'>
+        <div className='error-page-title'></div>
+      </div>
+      ErrorPage</div>
+  )
+}
+
+export default ErrorPage
