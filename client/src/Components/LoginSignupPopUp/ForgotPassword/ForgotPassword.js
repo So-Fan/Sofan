46cacbc1e44ca8a47d630860d87525428c8bc6e3
Ra <rami@jwcorp.fr>
2023-07-30 06:42:25 +0200

    onchange regex mail modify + disable button and hover + active when regex not good

--- client/src/Components/LoginSignupPopUp/ForgotPassword/ForgotPassword.js
+++ client/src/Components/LoginSignupPopUp/ForgotPassword/ForgotPassword.js
@@ -1,56 +1,61 @@
 import React, { useState } from "react";
 import "./ForgotPassword.css";
 import Button from "../../Button/Button";
+
 function ForgotPassword() {
+
   const [email, setEmail] = useState("");
-  const [emailRegexError, setEmailRegexError] = useState(false);
-  const [emailError, setEmailError] = useState(false);
+  const [isValidEmail, setIsValidEmail] = useState(false);
 
   function handleEmailChange(event) {
     const emailValue = event.target.value;
     setEmail(emailValue);
+    
     const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
-    setEmailRegexError(!emailRegex.test(emailValue));
+    const isValid = emailRegex.test(emailValue);
+    setIsValidEmail(isValid);
   }
-  function handleMailInput(e) {
-    const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
-    if (emailRegex.test(e.target.value)) {
-      setEmailError(false);
-    } else {
-      setEmailError(true);
-    }
-  }
-  
+
   return (
     <div className="forgot-password-popup-container">
+
       <div className="forgot-password-popup-wrap">
+
         <div className="forgot-password-popup-title">
           Rénitialisez votre mot de passe
         </div>
+
         <div className="forgot-password-popup-description-and-input-container">
+        
           <div className="forgot-password-popup-description">
             Entrez votre mail pour trouver nous aider à retrouver votre compte
           </div>
-          {/* <div className="forgot-password-popup-input"> */}
-            <input
-              className="forgot-passord-popup-mail-input"
-              type="Email"
-              value={email}
-              onChange={handleEmailChange}
-              onBlur={handleMailInput}
-              placeholder="Entrez votre mail"
-              name=""
-              id=""
-            />
-            {emailError && (
-              <p className="signup-user-error-mail">
-                Veuillez entrer une adresse e-mail valide.
-              </p>
-            )}
-          {/* </div> */}
+
+          <input 
+            className="forgot-passord-popup-mail-input"
+            type="Email"
+            value={email}
+            onChange={handleEmailChange}
+            placeholder="Entrez votre mail"
+          />
+          
+          { email && !isValidEmail && 
+            <p className="signup-user-error-mail">
+              Veuillez entrer une adresse e-mail valide.
+            </p>
+          }
+
         </div>
-        <button className="forgot-password-popup-validation-button">Valider</button>
+
+        <button
+          className="forgot-password-popup-validation-button"
+          disabled={!isValidEmail} 
+        >
+          Valider
+        </button>
+
       </div>
+
     </div>
   );
 }
