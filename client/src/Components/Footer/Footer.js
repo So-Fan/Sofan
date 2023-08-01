79080184547c44605b541028be75a5fcc165ec2b
shajeed <shajeed@live.fr>
2023-07-31 21:33:40 -0400

    Footer social links

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './Footer.css';
+import SocialLinks from './SocialLinks/SocialLinks';
+import InstagramLogo from '../../Assets/Image/SocialIcons/instagram.svg'
+import LinkedInLogo from '../../Assets/Image/SocialIcons/linkedin.svg'
+
+const links = [
+    { icon: InstagramLogo, url: 'https://www.instagram.com/sofan_app/' },
+    { icon: LinkedInLogo, url: 'https://www.linkedin.com/company/sofan-app/' }
+  ];
 
 function Footer() {
     return (
-        <div className='footer-container'>
-            Footer
-        </div>
+        <footer className='footer-container'>
+            <SocialLinks links={links} />
+            <div className='copyright-section'>
+                <p>&copy; 2023 Copyright: Sofan</p>
+            </div>
+        </footer>
     );
 }
 
