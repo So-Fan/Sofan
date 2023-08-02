1f2563c69bc8a222fb14ea00d08a59f0e00bbca7
shajeed <shajeed@live.fr>
2023-08-01 23:56:22 -0400

    signout fix

--- client/src/Components/Navbar/NavProfile/NavProfile.js
+++ client/src/Components/Navbar/NavProfile/NavProfile.js
@@ -1,53 +1,72 @@
 import React, { useContext } from "react";
 import "./NavProfile.css";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 import { auth } from "../../../Configs/firebase";
-import { signOut } from 'firebase/auth';
-import UserContext from '../../../UserContext';
+import { signOut } from "firebase/auth";
+import UserContext from "../../../UserContext";
 import useEth from "../../../contexts/EthContext/useEth";
 import { useNavigate } from "react-router-dom";
 
 const NavProfile = ({ web3auth, isProfileClicked, src, userInfo = null }) => {
   const { setLoggedInUser, setLocalWeb3authProvider } = useContext(UserContext);
   const navigate = useNavigate();
-  const {
-    setWeb3authProvider,
-  } = useEth();
+  const { setWeb3authProvider } = useEth();
 
-  const handleSignOut = async() => {
+  const handleSignOut = async () => {
+    console.log("sign out");
     setWeb3authProvider(null);
+    signOut(auth)
+      .then(() => {
+        setLoggedInUser(null);
+        setLocalWeb3authProvider(null);
+        localStorage.removeItem("loggedInUser");
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
     try {
       if (!web3auth) return;
       await web3auth.logout();
     } catch (err) {
       console.error(err);
     }
-    signOut(auth)
-    .then(() => {
-      setLoggedInUser(null);
-      setLocalWeb3authProvider(null);
-        localStorage.removeItem("loggedInUser");
-      navigate('/')
-    })
-      .catch((error) => {
-        console.log(error);
-      });
-    
   };
 
   return (
-    <div className="navbar-navprofile-container" >
+    <div className="navbar-navprofile-container">
       <div className="navbar-navprofile" href="/my-profile">
-        <img id="navbar-user-profile-img" src={src} alt={`${src.split("/").pop().split(".")[0]} image`} />
+        <img
+          id="navbar-user-profile-img"
+          src={src}
+          alt={`${src.split("/").pop().split(".")[0]} image`}
+        />
       </div>
-      {isProfileClicked && <div className="navbar-dropdown">
-        <Link to={userInfo.account_type !== 'free' ? `/athleteprofile/${userInfo.id}` : `/userprofile/${userInfo.id}`}>Voir profil</Link>
-        <Link to="/settings">Settings</Link>
-        <Link to="/legals">Mentions<br />légales</Link>
-        <Link onClick={handleSignOut} 
-        // to="/"
-        >Déconnecter</Link>
-      </div>}
+      {isProfileClicked && (
+        <div className="navbar-dropdown">
+          <Link
+            to={
+              userInfo.account_type !== "free"
+                ? `/athleteprofile/${userInfo.id}`
+                : `/userprofile/${userInfo.id}`
+            }
+          >
+            Voir profil
+          </Link>
+          <Link to="/settings">Settings</Link>
+          <Link to="/legals">
+            Mentions
+            <br />
+            légales
+          </Link>
+          <Link
+            onClick={handleSignOut}
+            // to="/"
+          >
+            Déconnecter
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
