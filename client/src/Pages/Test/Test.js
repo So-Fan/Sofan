8394aba3a3373e2af28569e968b51f3b6b883a1d
JWMatheo <matmatvallone8@gmail.com>
2023-08-16 04:09:40 +0200

    create data folder and constant.js + create add fund to wallet component

--- client/src/Pages/Test/Test.js
+++ client/src/Pages/Test/Test.js
@@ -2,15 +2,13 @@ import React, { useEffect, useState, useCallback } from "react";
 import "./Test.css";
 import PopUpBlockchainError from "../../Components/PopUpBlockchainError/PopUpBlockchainError";
 import Modal from "../../Components/Modal/Modal";
+import PopUpAddFundToWallet from "../../Components/PopUpAddFundToWallet/PopUpAddFundToWallet";
 const Test = () => {
   return (
     <>
-      <input
-        type="number"
-        name=""
-        id=""
-        style={{ position: "absolute", top: "250px" }}
-      />
+      <Modal style={{ display: "none" }}>
+        <PopUpAddFundToWallet />
+      </Modal>
     </>
   );
 };
---
+++ client/src/data/constants.js
@@ -0,0 +1,16 @@
+export const Warning =
+  "https://firebasestorage.googleapis.com/v0/b/sofan-app.appspot.com/o/assets%2FVectorClose.svg?alt=media&token=9cce0dfe-7df1-48f5-a1dd-7fd9892cd99a";
+
+export const DefaultYellowButtonPopUp = {
+  width: "410px",
+  height: "56px",
+  backgroundColor: "#F6D463",
+  borderRadius: "10px",
+  color: "#000",
+  fontFamily: "britanica-heavy",
+  fontSize: "20px",
+  fontStyle: "normal",
+  fontWeight: "400",
+  lineHeight: "normal",
+  border: "none",
+};
