a672d5ed79e4c4cf2527f5220fb21620161a565a
JWMatheo <matmatvallone8@gmail.com>
2023-09-28 21:19:23 +0200

    dynamic chain setup for alchemy

--- client/src/Configs/alchemy.js
+++ client/src/Configs/alchemy.js
@@ -1,9 +1,21 @@
-import { Network, Alchemy, NftFilters } from "alchemy-sdk";
+import { Network, Alchemy } from "alchemy-sdk";
 
-const settings = {
-  apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
-  network: Network.ETH_GOERLI,
-  maxRetries: 10,
-};
+const currentBlockchain = process.env.REACT_APP_BLOCKCHAIN;
+
+let settings;
+if (currentBlockchain === "mainnet") {
+  settings = {
+    apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
+    network: Network.ETH_MAINNET,
+    maxRetries: 10,
+  };
+} else if (currentBlockchain === "goerli") {
+  settings = {
+    apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
+    network: Network.ETH_GOERLI,
+    maxRetries: 10,
+  };
+}
+
 const alchemy = new Alchemy(settings);
 export default alchemy;
