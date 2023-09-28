4cc8180576aa13204482a301670a76fbf5e56c20
JWMatheo <matmatvallone8@gmail.com>
2023-09-28 03:56:02 +0200

    Improve UX flow for crossmint payment. Create CrossmintPayload

---
+++ client/src/contexts/CrossmintPayloadContext/useCrossmintPayload.js
@@ -0,0 +1,6 @@
+import { useContext } from "react";
+import CrossmintPayloadContext from "./CrossmintPayloadContext";
+
+const useCrossmintPayloadContext = () => useContext(CrossmintPayloadContext);
+
+export default useCrossmintPayloadContext;
