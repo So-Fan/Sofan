a2f36d80ba4e63a247c132625aed2a7267ed1a1f
Ra <rami@jwcorp.fr>
2023-09-29 20:56:18 +0200

    remove local storage

--- client/src/Components/LoginSignupPopUp/PasswordReset/PasswordReset.js
+++ client/src/Components/LoginSignupPopUp/PasswordReset/PasswordReset.js
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
-import './PasswordReset.css'
-import { verifyPasswordResetCode, confirmPasswordReset } from "firebase/auth";  // Import Firebase auth methods
-import { auth } from '../../../Configs/firebase';
+import React, { useState } from "react";
+import "./PasswordReset.css";
+import { verifyPasswordResetCode, confirmPasswordReset } from "firebase/auth"; // Import Firebase auth methods
+import { auth } from "../../../Configs/firebase";
 
 function PasswordReset() {
-  const [newPassword, setNewPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [message, setMessage] = useState('');
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [message, setMessage] = useState("");
   const [isPasswordChanged, setIsPasswordChanged] = useState(false);
 
   const urlParams = new URLSearchParams(window.location.search);
-  const oobCode = urlParams.get('oobCode');  // This is the token from the URL
+  const oobCode = urlParams.get("oobCode"); // This is the token from the URL
 
   function validatePassword(password) {
     if (!password) {
@@ -39,6 +39,8 @@ function PasswordReset() {
       await confirmPasswordReset(auth, oobCode, newPassword);
       setMessage("Mot de passe réinitialisé avec succès!");
       setIsPasswordChanged(true);
+      localStorage.removeItem("loggedInUser");
+      localStorage.removeItem("isUserLogged");
     } catch (error) {
       setMessage("Erreur lors de la réinitialisation du mot de passe.");
       console.error(error);
@@ -53,13 +55,33 @@ function PasswordReset() {
         <>
           <h1>Changer le mot de passe</h1>
           <form onSubmit={handlePasswordReset}>
-            <label htmlFor="new-password">Nouveau mot de passe:</label><br />
-            <input type="password" id="new-password" name="new-password" required onChange={(e) => setNewPassword(e.target.value)} /><br /><br />
+            <label htmlFor="new-password">Nouveau mot de passe:</label>
+            <br />
+            <input
+              type="password"
+              id="new-password"
+              name="new-password"
+              required
+              onChange={(e) => setNewPassword(e.target.value)}
+            />
+            <br />
+            <br />
 
-            <label htmlFor="confirm-password">Confirmer le mot de passe:</label><br />
-            <input type="password" id="confirm-password" name="confirm-password" required onChange={(e) => setConfirmPassword(e.target.value)} /><br /><br />
+            <label htmlFor="confirm-password">Confirmer le mot de passe:</label>
+            <br />
+            <input
+              type="password"
+              id="confirm-password"
+              name="confirm-password"
+              required
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+            <br />
+            <br />
 
-            <button type="submit" className="change-password-button">Changer le mot de passe</button>
+            <button type="submit" className="change-password-button">
+              Changer le mot de passe
+            </button>
           </form>
           {message && <p>{message}</p>}
         </>
